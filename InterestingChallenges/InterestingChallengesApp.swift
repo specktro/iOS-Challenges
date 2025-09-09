548a3831c7c9a34581fcd78ecadbbb6cb5aa6114
@specktro <specktro@nonull.mx>
2025-09-08 20:09:03 -0600

    Initial commit with simple .gitignore file and iOS App template

---
+++ InterestingChallenges/InterestingChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestingChallengesApp.swift
+//  InterestingChallenges
+//
+//  Created by specktro on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct InterestingChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterestingChallengesTests/InterestingChallengesTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterestingChallengesTests.swift
+//  InterestingChallengesTests
+//
+//  Created by specktro on 08/09/25.
+//
+
+import Testing
+@testable import InterestingChallenges
+
+struct InterestingChallengesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
